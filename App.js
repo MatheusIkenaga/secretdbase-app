06d0332080f6bb1905f30ef8ae9776928570f904
Matheus Ikenaga <51059596+MatheusIkenaga@users.noreply.github.com>
2022-04-08 14:53:53 -0300

    Testing export to create apk

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import * as React from 'react'
-import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
-
 import Routes from './src/routes'
 
 export default function App() {
   return (
-      <Routes/>
+    <Routes/>
   );
 }
