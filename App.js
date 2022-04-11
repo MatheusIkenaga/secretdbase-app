954663633c3f768748096b9b358c1a37c782bc08
Matheus Ikenaga <51059596+MatheusIkenaga@users.noreply.github.com>
2022-04-11 15:28:32 -0300

    Changing app.json to build apk fixed

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import * as React from 'react'
-import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
-
+import * as SplashScreen from 'expo-splash-screen';
 import Routes from './src/routes'
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
       <Routes/>
   );
